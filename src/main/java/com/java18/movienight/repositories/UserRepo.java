a3df6a0fdf2849f89a26b25bebcfc12ecf42ade9
Johan <johanwiren1@gmail.com>
2019-12-03 14:02:02 +0100

    Test adding user to mongoDB atlas

---
+++ src/main/java/com/java18/movienight/repositories/UserRepo.java
@@ -0,0 +1,12 @@
+package com.java18.movienight.repositories;
+
+import com.java18.movienight.entities.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends MongoRepository<User, ObjectId> {
+  public User findByUsername(String username);
+  public User findBy_id(ObjectId _id);
+}
---
+++ src/main/java/com/java18/movienight/services/UserService.java
@@ -0,0 +1,30 @@
+package com.java18.movienight.services;
+
+import com.java18.movienight.entities.User;
+import com.java18.movienight.repositories.UserRepo;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+  @Autowired
+  private UserRepo userRepo;
+
+  public User findByUsername(String username) {
+    return userRepo.findByUsername(username);
+  }
+
+  public User findBy_id(ObjectId _id) {
+    return userRepo.findBy_id(_id);
+  }
+
+  public User insertUser(User user) {
+    return userRepo.save(user);
+  }
+
+  public void deleteUser(User user) {
+    userRepo.delete(user);
+  }
+}
