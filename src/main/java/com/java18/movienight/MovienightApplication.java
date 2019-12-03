c5a4f74a8bfa9a74c6bbf2e9078521c326de850d
Anders Clark <anders.clark@gmail.com>
2019-12-03 11:57:32 +0100

    init

---
+++ src/main/java/com/java18/movienight/MovienightApplication.java
@@ -0,0 +1,13 @@
+package com.java18.movienight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovienightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovienightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java18/movienight/MovienightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java18.movienight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovienightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
