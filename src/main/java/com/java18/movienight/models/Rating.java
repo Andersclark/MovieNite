ce2d120c7c3f1c771ac05460a9daba0ff43f63b4
Anders Clark <anders.clark@gmail.com>
2019-12-04 11:28:56 +0100

    Rating entity for array of ratings inside Movie-entity.

---
+++ src/main/java/com/java18/movienight/models/Rating.java
@@ -0,0 +1,14 @@
+package com.java18.movienight.models;
+
+
+public class Rating {
+    public String source;
+    public String rating;
+
+    public Rating(){}
+
+    public Rating(String source, String rating) {
+        this.source = source;
+        this.rating = rating;
+    }
+}
