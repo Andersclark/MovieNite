b4d884c8e6233a1bcab0f1dd1c9f71005068e690
Anton <antonstromkarl@gmail.com>
2019-12-05 15:00:09 +0100

    card for separate movie added

---
+++ src/main/resources/static/src/components/movieCard.js
@@ -0,0 +1,8 @@
+export default{
+  template: `
+  <div class="movie-card">
+      <img class="movie-poster" src="https://assets.mubi.com/images/notebook/post_images/22267/images-w1400.jpg?1474980339" alt="poster"/>
+      <h5>{MOVIE TITLE}</h5>
+      <button type="submit">Book</button>
+  </div>`
+}
