52989f764361895990276528711bb57f104a8db4
Anton <antonstromkarl@gmail.com>
2019-12-05 13:15:37 +0100

    added hamburger to toggle sidedrawer in mobile view

--- src/main/resources/static/src/components/navBar.js
+++ src/main/resources/static/src/components/navBar.js
@@ -1,11 +1,16 @@
 export default {
-	template: `
+  template: `
+  <div>
+  <div class="hamburger">
+      <i class="fas fa-bars"></i>
+  </div>
   <div class="container">
     <div class="row">
       <div class="header">
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>  
+        <router-link style="margin-right: 10px" to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
       </div>
     </div>
+  </div>
   </div>`,
 };
