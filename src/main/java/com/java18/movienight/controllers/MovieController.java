cb446cf4b7ac847e96e4fd6c3ae6ea7d1a6d246f
Anders Clark <anders.clark@gmail.com>
2019-12-06 13:07:09 +0100

    Added apidoc view to navbar and router

--- src/main/java/com/java18/movienight/controllers/MovieController.java
+++ src/main/java/com/java18/movienight/controllers/MovieController.java
@@ -41,7 +41,7 @@ public class MovieController {
             results = omdbService.searchByTitleContaining(title);
             results.setSearchString(title);
         }
-        if( results.getMovies().isEmpty()){
+        if( results.getMovies().isEmpty() || results == null || results.getMovies() == null){
            response = new ResponseEntity(HttpStatus.NOT_FOUND);
         } else {
             atlasService.saveSearchResults(results);
