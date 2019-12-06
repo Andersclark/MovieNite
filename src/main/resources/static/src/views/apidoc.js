6013b6c74585ba372ed4bffaa22feee86896c5a7
Anders Clark <anders.clark@gmail.com>
2019-12-06 13:03:50 +0100

    Added apidoc view to navbar and router

---
+++ src/main/resources/static/src/views/apidoc.js
@@ -0,0 +1,16 @@
+
+export default {
+    template: `
+    <div id="apidoc">
+        <div class="container">
+            <div class="row">
+                <div class="one-half">
+                     <h3>MovieKnight API documentation</h3>
+                      <p>MovieKnight is both a user-friendly webapp for booking private viewings with friends at your home and an API serving data on movies.</p>
+                      <p>Our database is in itself a cache of data our users request, which are then collected from the <a href="">Open Movie Database, OMDB</a>, which is pretty slow.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  `,
+}
