7ee3c34513d2120deaa7075e363912d50c269a42
Anders Clark <anders.clark@gmail.com>
2019-12-06 09:47:30 +0100

    Caching of omdb-searchresult to Atlas. Check for cached searchresults before getting from omdb.

--- src/main/java/com/java18/movienight/controllers/MovieController.java
+++ src/main/java/com/java18/movienight/controllers/MovieController.java
@@ -1,18 +1,14 @@
 package com.java18.movienight.controllers;
 
 import com.java18.movienight.models.Movie;
-import com.java18.movienight.models.MoviePreview;
-import com.java18.movienight.models.OmdbSearchResult;
+import com.java18.movienight.models.SearchResult;
 import com.java18.movienight.services.AtlasService;
 import com.java18.movienight.services.OmdbService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -37,10 +33,22 @@ public class MovieController {
 
     @GetMapping("/search")
     private ResponseEntity searchByTitleContaining(@RequestParam String title) {
-        OmdbSearchResult results = omdbService.searchByTitleContaining(title);
-        if(results.getMovies().isEmpty()){
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        } else { return new ResponseEntity<>(results.getMovies(), HttpStatus.OK); }
+        ResponseEntity response;
+        SearchResult results = new SearchResult();
+        results = atlasService.findBySearchString(title);
+
+        if(results == null){
+             results = omdbService.searchByTitleContaining(title);
+             results.setSearchString(title);
+             atlasService.saveSearchResults(results);
+        }
+        if( results.getMovies().isEmpty()){
+           response = new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            response = new ResponseEntity<>(results.getMovies(), HttpStatus.OK);
+        }
+
+        return response;
     }
 
 
--- src/main/java/com/java18/movienight/models/SearchResult.java
+++ src/main/java/com/java18/movienight/models/SearchResult.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -13,12 +15,16 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class OmdbSearchResult implements Serializable {
+public class SearchResult implements Serializable {
+
+    @Id
+    ObjectId _id;
+    public String searchString;
 
     @JsonProperty("Search")
     private ArrayList<MoviePreview> movies;
 
-    public OmdbSearchResult() {
+    public SearchResult() {
         movies = new ArrayList<>();
     }
 
