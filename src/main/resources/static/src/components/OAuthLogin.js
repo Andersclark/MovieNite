0cd959ecef58c2badc426d41901832dbab8bc48d
Johan <johanwiren1@gmail.com>
2019-12-10 16:04:42 +0100

    Re-enabled google login function

---
+++ src/main/resources/static/src/components/OAuthLogin.js
@@ -0,0 +1,41 @@
+export default {
+  template: `
+    <button v-if="displayLoginButton" id="signinButton" @click="onSignInClick">Link my Google Calendar</button>
+  `,
+  data() {
+    return {
+      displayLoginButton: true
+    }
+  },
+  methods: {
+    onSignInClick() {
+      this.$auth2.grantOfflineAccess().then(this.signInCallback);
+    },
+    async signInCallback(authResult) {
+      console.log('authResult', authResult);
+    
+      if (authResult['code']) {
+    
+        // Hide the sign-in button now that the user is authorized
+        this.displayLoginButton = false;
+    
+        // Send the code to the server
+        let result = await fetch('/auth/storeauthcode', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/octet-stream; charset=utf-8',
+            'X-Requested-With': 'XMLHttpRequest',
+          },
+          body: authResult['code']
+        });
+        
+        console.log('/storeauthcode:', await result.json());
+        
+      } else {
+        // There was an error.
+        console.error('OAuth error');
+        
+      }
+    }
+  }
+}
